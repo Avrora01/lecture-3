4143381cc09df8b18265776d88346b5ecf75ea7f
avrora01 <sabrinasuhrobzoda01@gmail.com>
2023-05-17 15:47:26 +0430

    text

--- index.js
+++ index.js
@@ -11,6 +11,59 @@
 
 
 //task2
-function booltoSrting(n){
-     let 
-}
+// function booltoSrting(n){ 
+//      return n.toString()
+// }
+// console.log(booltoSrting(true))
+
+
+
+
+//task3
+// function booltoSrting(n){ 
+//      return Number(n)
+// }
+// console.log(booltoSrting("121"))
+// console.log(booltoSrting("141"))
+// console.log(booltoSrting("161"))
+
+
+//task4
+// function string(n){
+//      if(n=="Mubashir"){
+//         return "Hello, my Love!"
+//      }
+//      else{
+//         return `Hello, ${n}!`
+//      }
+// }
+// console.log( string("Mubashir"))
+// console.log( string("Bob"))
+// console.log( string("Helen"))
+
+
+
+//task5
+// function sum(a, b)
+// {
+//     return a.length==b.length
+// }
+// console.log(sum("AA","BB"));
+// console.log(sum('ddd', 'fgdg'));
+
+
+
+//task6
+// let cnt=0;
+// function sum(n)
+// {
+// if ( n.length == 0)
+// {
+//     return cnt
+// }
+// let m = n.slice(1)
+// cnt ++
+// return sum(m)
+// }
+// console.log(sum("apple"));
+// console.log(sum("sabrina"));
