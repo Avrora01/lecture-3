0b028bfdf15c173d5216bcd3033d892e54be6985
avrora01 <sabrinasuhrobzoda01@gmail.com>
2023-05-17 12:27:00 +0430

    text

---
+++ index.js
@@ -0,0 +1,16 @@
+//task1
+// function string(n){
+//      let text=""
+//      return text.concat("something",n)
+// }
+// console.log( string(" is better than nothing"))
+// console.log( string(" Bob John"))
+// console.log( string(" something"))
+
+
+
+
+//task2
+function booltoSrting(n){
+     let 
+}
